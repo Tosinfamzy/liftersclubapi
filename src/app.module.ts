8948581b949f72420a8a0533e3e2d619595a4c71
Tosinfamzy <famurewatosin@gmail.com>
2024-04-22 19:30:49 +0100

    Add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: process.env.NODE_ENV === 'development' ? true : false, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
