665ea1bc49ef58f16e23644933adae79f58e04e6
Tosin Famurewa <famurewatosin@gmail.com>
2024-08-29 17:39:48 +0100

    Add some description to swagger

--- src/excercises/dto/create-excercise.dto.ts
+++ src/excercises/dto/create-excercise.dto.ts
@@ -1,15 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateExcerciseDto {
+  @ApiProperty({ description: 'Name of the exercise' })
+  @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'type of exercise eg Cardio | strength etc' })
+  @IsString()
   readonly type: string;
 
+  @ApiProperty({ description: 'Muscle Group' })
+  @IsString()
   readonly muscle: string;
 
+  @ApiProperty({ description: 'Name of gym equipment' })
+  @IsString()
   readonly equipment: string;
 
+  @ApiProperty({ description: 'Diffculty of exercise' })
+  @IsString()
   readonly difficulty: string;
 
+  @ApiProperty({ description: 'How to perform exercise' })
+  @IsString()
+  @IsOptional()
   readonly instructions?: string;
 
+  @ApiProperty({ description: 'Url to video of excercise' })
+  @IsString()
+  @IsOptional()
   readonly video?: string;
 }
